be2c9abe1901ab4b1cf06d13aafe1d7fe6e69bda
lenlino <65199014+lenlino@users.noreply.github.com>
2024-09-02 15:45:11 +0900

    add stock manager

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import discord
 from discord.ext import tasks
 from pydactyl import PterodactylClient
 from dotenv import load_dotenv
+import mysql.connector
 
 load_dotenv()
 
@@ -28,14 +29,21 @@ async def job():
         description="5分おきに更新されます。",
         color=discord.Colour.blurple(),
     )
+    node_details = []
     for page in nodes:
         for node in page:
             if node['attributes']['location_id'] not in location_ids:
                 continue
             print(type(node))
             all_mem_size = node['attributes']['memory']
+            all_disk_size = node['attributes']['disk']
             allocated_mem_size = node['attributes']['allocated_resources']['memory']
+            allocated_disk_size = node['attributes']['allocated_resources']['disk']
             non_allocated_mem_size = all_mem_size - allocated_mem_size
+            non_allocated_disk_size = all_disk_size - allocated_disk_size
+
+            node_details.append((non_allocated_mem_size, non_allocated_disk_size))
+
             node_name = node['attributes']['name']
 
             status_icon = "✅"
@@ -47,6 +55,25 @@ async def job():
 
     await ((await bot.get_channel(1234306814305108068).fetch_message(1234309444360343552))
            .edit(embed=embed))
+
+    conn = mysql.connector.connect(host=os.environ.get('DB_HOST', None), user=os.environ.get('DB_USER', None)
+                                   , password=os.environ.get('DB_PASS', None))
+    curs = conn.cursor()
+    curs.execute('SELECT * FROM tblproducts WHERE gid=1')
+    for row in curs.fetchall():
+        disk_size = row[7]
+        mem_size = row[8]
+        is_enable = False
+        for node in node_details:
+            if mem_size <= node[0] and disk_size <= node[1]:
+                is_enable = True
+                break
+        if is_enable:
+            curs.execute(f'UPDATE tplproducts SET stockcontrol=0 WHERE id={row[0]}')
+        else:
+            curs.execute(f'UPDATE tplproducts SET stockcontrol=1 WHERE id={row[0]}')
+
+
 
 
 bot.run(discord_token)
